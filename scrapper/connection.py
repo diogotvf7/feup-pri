5ca4d7e0f3daa5328ae0d8a24a65f0364abd439e
diogotvf7 <diogotvf7@gmail.com>
2024-09-28 17:22:37 +0100

    Reject cookies

--- scrapper/connection.py
+++ scrapper/connection.py
@@ -1,17 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from database.database import Database
 
+articles = [
+    "https://finance.yahoo.com/video/brian-cornells-plan-target-back-095539854.html",
+    "https://finance.yahoo.com/news/target-ceo-hopes-the-company-will-eventually-remove-locked-cases-as-it-combats-retail-theft-130049971.html",
+]
+
+def reject_cookies():
+    if "consent.yahoo.com" in driver.current_url:
+        print("Rejecting cookies...")
+        reject_button = driver.find_element(By.XPATH, "//button[@name='reject']")
+        reject_button.click()
+        print("Cookies rejected.")
+    else:
+        print("No cookies to reject.")
+
+database = Database()
+
+print("Starting the web driver...")
 driver = webdriver.Firefox()
+print("Web driver started.")
 
-driver.get("https://finance.yahoo.com/video/brian-cornells-plan-target-back-095539854.html")
+driver.get(articles[0])
 
-driver.implicitly_wait(5)
+print("Waiting for the page to load...")
+driver.implicitly_wait(2)
 
-page_html = driver.page_source
-print(page_html)
+reject_cookies()
 
-title = driver.find_element(By.XPATH, "//h1").text
-print(title)
 
-title = driver.title
-print(title)
+
+
+driver.implicitly_wait(0.5)
+
