b52032d1c389470457f9762dcc48101624d8d540
diogotvf7 <diogotvf7@gmail.com>
2024-10-01 11:21:09 +0100

    Changed a selector

--- scrapper/connection.py
+++ scrapper/connection.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from scraper import get_article_information
+from scraper import get_article_information, reject_cookies
 from database.database import Database
 
 articles = [
@@ -8,16 +8,7 @@ articles = [
     "https://finance.yahoo.com/news/target-ceo-hopes-the-company-will-eventually-remove-locked-cases-as-it-combats-retail-theft-130049971.html",
 ]
 
-def reject_cookies():
-    if "consent.yahoo.com" in driver.current_url:
-        print("Rejecting cookies...")
-        reject_button = driver.find_element(By.XPATH, "//button[@name='reject']")
-        reject_button.click()
-        print("Cookies rejected.")
-    else:
-        print("No cookies to reject.")
-
-#database = Database()
+# database = Database()
 
 print("Starting the web driver...")
 driver = webdriver.Firefox()
@@ -28,7 +19,7 @@ driver.get(articles[0])
 print("Waiting for the page to load...")
 driver.implicitly_wait(2)
 
-reject_cookies()
+reject_cookies(driver)
 get_article_information(driver)
 
 
--- scrapper/scraper.py
+++ scrapper/scraper.py
@@ -1,7 +1,13 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 css_selector_for_article_paragraphs = ".article > div:nth-child(1) > div:nth-child(3) > div:nth-child(3)"
 
+
+# class Scraper:
+#     def __init__(self):
+#         self.driver = webdriver.Firefox()
+        
 
 # TODO: Add this information to the database
 def get_article_information(driver):
@@ -16,12 +22,19 @@ def get_article_information(driver):
 
     article_date = driver.find_element(By.CLASS_NAME, "byline-attr-meta-time").text
     print("Article Data: " + article_date)
-
-    article_body = driver.find_element(By.CSS_SELECTOR, css_selector_for_article_paragraphs)
+    article_body = driver.find_element(By.CLASS_NAME, "body")
     paragraphs = article_body.find_elements(By.XPATH, ".//p")
     article_text = ""
     for paragraph in paragraphs:
         article_text += paragraph.text + " " 
+
     print("Article Body: " + article_text)
 
-    
+def reject_cookies(driver):
+    if "consent.yahoo.com" in driver.current_url:
+        print("Rejecting cookies...")
+        reject_button = driver.find_element(By.XPATH, "//button[@name='reject']")
+        reject_button.click()
+        print("Cookies rejected.")
+    else:
+        print("No cookies to reject.")
