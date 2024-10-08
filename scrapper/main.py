dcb7b701da825934eea4ce2ec594679de3fb0c93
Hélder <helder7_gsc@outlook.com>
2024-10-08 17:18:23 +0100

    Dealing with scroll loading of articles

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,17 +1,16 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import os, spiders, json
 
 os.makedirs("database", exist_ok=True)
 
 
 homepage = "https://finance.yahoo.com/topic/stock-market-news"
-articles_links = [
-
-]
+articles_links = []
 
 stocks_links = [
     "https://finance.yahoo.com/quote/AMZN/",
@@ -29,11 +28,37 @@ spiders.reject_cookies(driver)
 articles = []
 stocks = []
 stock_changes = []
+curr_scroll = 2000
 
-
-articles_ul = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, '.My\\(0\\).P\\(0\\).Wow\\(bw\\).Ov\\(h\\)'))
+articles_ul = WebDriverWait(driver, 10).until(
+    expected_conditions.presence_of_element_located((By.CSS_SELECTOR, '.My\\(0\\).P\\(0\\).Wow\\(bw\\).Ov\\(h\\)'))
 )
+
+def get_number_of_li_elements(ul_element):
+    return len(ul_element.find_elements(By.TAG_NAME, 'li'))
+
+prev_n_articles = get_number_of_li_elements(articles_ul)
+
+while True:
+    try:
+        driver.execute_script(f'window.scrollTo(0,{curr_scroll})')
+        #print('dei scroll')
+        curr_scroll += 2000
+
+        WebDriverWait(driver, 1).until(
+            lambda d: get_number_of_li_elements(articles_ul) > prev_n_articles
+        )
+
+        print(f"Articles: {get_number_of_li_elements(articles_ul)}")
+
+        prev_n_articles = get_number_of_li_elements(articles_ul)
+
+    except TimeoutException:
+        print("No articles loaded, leaving while loop.")
+        break
+    
+    
+#print('acabei os scrolls')
 
 articles_li = articles_ul.find_elements(By.TAG_NAME, 'li')
 
@@ -46,11 +71,13 @@ for article in articles_li:
         next
 
 print(articles_links)
-for stock_url in stocks_links:
-    stock = spiders.read_stock(driver, stock_url)
-    stocks.append(stock)
+print(len(articles_links))
+print(len(articles_links) != len(set(articles_links)))
 
 
+# for stock_url in stocks_links:
+#     stock = spiders.read_stock(driver, stock_url)
+#     stocks.append(stock)
 
 # for article_url in articles_links:
 #     article = spiders.read_article(driver, article_url)
