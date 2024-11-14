edd38a92dcebd68e894502630c1c66235a202041
Diogo Tomás Valente Fernandes <up202108752@edu.fe.up.pt>
2024-11-14 23:57:12 +0000

    queries

--- database/queries/scripts/query3.py
+++ database/queries/scripts/query3.py
@@ -1,14 +1,22 @@
 import requests
 
-# Phrase match
 def query3():
     response = requests.get(
         "http://localhost:8983/solr/stocks/select",
         params={
-            "q": "title:\"stock market\"~2",
+            "defType": "edismax",
+            "qf": "title^3 body^2 authors",
+            "pf": "title^3 author^2 body",
             "indent": "true",
-            "q.op": "OR"
+            "q.op": "AND",
+            "sort":"created_at desc",
+
+            "q": "US presidential race",
         }
     )
 
     return response
+
+if __name__ == "__main__":
+    response = query3()
+    print(response.text)
--- database/queries/scripts/query4.py
+++ database/queries/scripts/query4.py
@@ -1,14 +1,25 @@
 import requests
 
-# Proximity
 def query4():
+    FAANG_TICKS = ["NVDA", "AAPL", "MSFT", "GOOG", "AMZN"]
+    FAANG = ["Nvidia", "Apple", "Microsoft", "Alphabet", "Amazon"]
+
     response = requests.get(
         "http://localhost:8983/solr/stocks/select",
         params={
-            "q": "\"stock market\"~5",
+            "defType": "edismax",
+            "qf": "title^3 body^2 authors",
+            "pf": "title^3 author^2 body",
             "indent": "true",
-            "q.op": "OR"
+            "q.op": "OR",
+            "sort":"created_at desc",
+
+            "q": " ".join(FAANG + FAANG_TICKS),
         }
     )
 
     return response
+
+if __name__ == "__main__":
+    response = query4()
+    print(response.text)
