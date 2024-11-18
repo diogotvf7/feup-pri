a2b6f8486237a74a390f2214bd240c752e93a02b
Vidal322 <pedrovidal2004@gmail.com>
2024-11-18 10:11:16 +0000

    queries updated

--- database/queries/scripts/newSchema/query1.py
+++ database/queries/scripts/newSchema/query1.py
@@ -1,9 +1,6 @@
 import requests
 
-def query4():
-    FAANG_TICKS = ["NVDA", "AAPL", "MSFT", "GOOG", "AMZN"]
-    FAANG = ["Nvidia", "Apple", "Microsoft", "Alphabet", "Amazon"]
-
+def query2():
     response = requests.get(
         "http://localhost:8983/solr/stocks/select",
         params={
@@ -11,15 +8,14 @@ def query4():
             "qf": "title^3 body^2 authors",
             "pf": "title^3 author^2 body",
             "indent": "true",
-            "q.op": "OR",
-            "sort":"created_at desc",
-
-            "q": " ".join(FAANG + FAANG_TICKS),
+            "q.op": "AND",
+            "sort": "created_at desc",
+            "q": "Apple stock forecast",
         }
     )
+    return response.json()
 
-    return response
 
 if __name__ == "__main__":
-    response = query4()
+    response = query1()
     print(response.text)
--- database/queries/scripts/newSchema/query4.py
+++ database/queries/scripts/newSchema/query4.py
@@ -1,6 +1,7 @@
 import requests
 
-def query1():
+def query4():
+
     response = requests.get(
         "http://localhost:8983/solr/stocks/select",
         params={
@@ -8,15 +9,15 @@ def query1():
             "qf": "title^3 body^2 authors",
             "pf": "title^3 author^2 body",
             "indent": "true",
-            "q.op": "AND",
+            "q.op": "OR",
             "sort":"created_at desc",
 
-            "q": "Tesla earnings report",
+            "q": "Why has NVIDIA stock skyrocketed in the past month?",
         }
     )
 
     return response
 
 if __name__ == "__main__":
-    response = query1()
+    response = query4()
     print(response.text)
