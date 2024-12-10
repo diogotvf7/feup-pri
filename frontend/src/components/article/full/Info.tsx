fb629e6b6b88654b5007613076f426d2be691f8c
Diogo Tomás Valente Fernandes <up202108752@edu.fe.up.pt>
2024-12-10 23:35:26 +0000

    Small update

--- frontend/src/components/article/full/Info.tsx
+++ frontend/src/components/article/full/Info.tsx
@@ -13,17 +13,19 @@ const Info = ({ article }: { article: article }) => {
         <p className="text-lg font-bold">Sentiment:</p>
         <Progress value={(article.sentiment_compound[0] + 1) * 50} />
       </div>
-      <div className="flex flex-wrap justify-end gap-2">
-        {article.stocks_changes_name?.map((stock, i) => (
-          <div key={i} className="flex flex-col text-xl">
-            <p className="text-lg font-bold">{stock}</p>
-            <p style={{ color: stock_change_color(article.stocks_changes_value[i]) }}>
-              {article.stocks_changes_value[i] > 0 ? '+' : ''}
-              {article.stocks_changes_value[i]}
-            </p>
-          </div>
-        ))}
-      </div>
+      {article.stocks_changes_name?.length > 0 && (
+        <div className="flex flex-wrap justify-end gap-2">
+          {article.stocks_changes_name?.map((stock, i) => (
+            <div key={i} className="flex flex-col text-xl">
+              <p className="text-lg font-bold">{stock}</p>
+              <p style={{ color: stock_change_color(article.stocks_changes_value[i]) }}>
+                {article.stocks_changes_value[i] > 0 ? '+' : ''}
+                {article.stocks_changes_value[i]}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
